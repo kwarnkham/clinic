19e1206dc61c65955ed61e1cb7c4b61dbf9dcb4a
KWARN KHAM <kwarnkham.moon@gmail.com>
2023-05-27 15:23:30 +0630

    daily stock

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\DB;
 
 class Kernel extends ConsoleKernel
 {
@@ -14,7 +15,19 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            $now = now();
+            DB::table('products')->where('item_id', '!=', 1)->orderBy('id')->lazy()->each(function ($product) use ($now) {
+                DB::table('product_stock')->insert([
+                    'product_id' => $product->id,
+                    'name' => $product->name,
+                    'stock' => $product->stock,
+                    'created_at' => $now,
+                    'updated_at' => $now
+                ]);
+            });
+        })
+            ->dailyAt('02:30');
     }
 
     /**
@@ -24,7 +37,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
